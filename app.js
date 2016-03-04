83768b9a86fe155f9b617241aa5180063b9f0daa
Jorge Goulart <datascan.ti@gmail.com>
2016-03-04 16:39:19 -0300

    Melhorando Chat realtime

--- app.js
+++ app.js
@@ -1,56 +1,110 @@
-var app = require('http').createServer(index)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-;
-app.listen(80, function() {
-  console.log("Servidor rodando!");
-});
+// var app = require('http').createServer(index)
+  // , io = require('socket.io').listen(app)
+  // , fs = require('fs')
+// ;
 
-function index(req, res){
-  fs.readFile(__dirname + '/index.html', function(err, data){
-	  res.writeHead(200);
-    res.end(data);
-  });
-};
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
+	// function index(req, res){
+	  // fs.readFile(__dirname + '/index.html', function(err, data){
+		  // res.writeHead(200);
+		// res.end(data);
+	  // });
+	// };
+
+	app.get('/', function(req, res){
+	  res.sendFile(__dirname + '/index.html');
+	});
 
 // Iniciando Socket.IO
 
 // var visitas = 0;
 // Evento connection ocorre quando entra um novo usuário.
 
-io.on('connection', function(socket){
-	socket.on('join', function (data) {
-		socket.join(data.email); // We are using room of socket io
-		console.log(data.email);
+	var clients = [];
+
+
+	io.on('connection', function(socket){
+		//console.log(socket);
+		
+		clients.push({"socket": socket.id, "nome": ''});
+		
+		for (i = 0; i < clients.length; i++) { 
+			if(clients[i].socket==socket.id && clients[i].nome==''){
+				socket.emit('toLogin', 'Insetir');
+			}
+		}
+		
+		
+
+		socket.on('join', function (data) {
+			for (i = 0; i < clients.length; i++) { 
+				if(clients[i].socket==socket.id){
+					clients[i].nome = data.email;
+					
+				}
+			}
+
+			// socket.join(data.email); // Sala
+			// console.log(data.email);
+			//console.log('User joined chat room 1');
+
+			// var roster = io.sockets.adapter.rooms;
+			// console.log(roster);
+			socket.emit('toList', clients);
+			socket.broadcast.emit('toList', clients);
+		});
+	  
+	  
+		setInterval(function(){ 
+		socket.emit('toClientData', Date());
+		
+
+		}, 1000);
+
+	  
+	  
+		socket.on('toServer', function(msg){
+
+			// Atualiza o total de visitas para os demais usuários.
+			io.sockets.in(msg.id_usuario).emit('toClient', {msg:msg.msg, id_usuario:msg.id_usuario});
+			console.log(msg.id_usuario + ":"+msg.msg);
+			// socket.emit('toClient', msg);
+			// socket.broadcast.emit('toClient', msg);
+		});
+		
+		
+		socket.on('disconnect', function(){
+
+			for (i = 0; i < clients.length; i++) { 
+				if(clients[i].socket==socket.id){
+					clients.splice(i, 1);
+				}
+			}
+			console.log('user disconnected: '+ socket.id);
+			socket.broadcast.emit('toList', clients);
+		});
+	  
 	});
-  
-  
-  setInterval(function(){ 
-	socket.emit('toClientData', Date());
 
-  }, 1000);
-  
-  
-  
-  socket.on('toServer', function(msg){
-  
-		// Atualiza o total de visitas para os demais usuários.
-		io.sockets.in(msg.email).emit('toClient', msg.msg);
-		console.log(msg.email);
-		// socket.emit('toClient', msg);
-		// socket.broadcast.emit('toClient', msg);
-  });
-  
-  // // Envia o total de visitas para os demais usuários.
-
-  // // socket.broadcast.emit('visits', visitas);
-
-  // // Evento disconnect ocorre quando sai um usuário.
-
-  // socket.on('disconnect', function(){
-    // visitas--;
-    // // Atualiza o total de visitas para os demais usuários.
-
-    // socket.broadcast.emit('message', visitas);
-  // });
-});
+	http.listen(3000, function() {
+	  console.log("Servidor rodando!");
+	});
+	
+	
+	
+	
+	// listarUsuario(){
+		// for (i = 0; i < clients.length; i++) { 
+				// if(clients[i].socket==socket.id){
+					// clients[i].nome = data.email;
+					
+				// }
+			// }
+	// }
+	
+	
